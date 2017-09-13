f1ef5b17411abe682b4348b536fc663184ae8c50
Caterina van Herpt <caterina.vanherpt@gmail.com>
2017-09-13 03:09:15 -0400

    final app

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -30,7 +30,7 @@ const Nav = () => {
 	return (
 		<nav className='nav'>
 			<h1>Grow<br/><span>Plant</span><br/>Grow</h1>
-			<button>Logout</button>
+			<img src="public/assets/logo.svg" alt=""/>
 		</nav>
 	)
 }
@@ -39,13 +39,17 @@ const Display = (props) => {
 	return (
 		<section className='displayPlants'>
 			{props.gardenEmpty ?
-				<h3>Keep track of your plants! <br/> Add plant info and images into the new plant form to get started...</h3>
+				<div className="background__image">
+					<h3>Keep track of your plants! <br/> Add plant info and images into the new plant form to get started...</h3>
+				</div>
 			:
-				<ul className="displayPlants__item" onClick={props.handleInfoCardDisplay}>
+				<ul className="displayPlants__item" >
 					{props.plantsArray.map((plant) => {
 						return (
-							<li key={plant.id}>
+							<li onClick={() => props.handleInfoCardDisplay(plant.id)} key={plant.id}>
+								<div className="img__overlay"></div>
 								<img src={plant.firebasePicPath} alt=""/>
+								<div className="img__border"></div>
 								<h4>{plant.name}</h4>
 							</li>
 						);
@@ -57,47 +61,94 @@ const Display = (props) => {
 }
 
 const PlantInfoSection = (props) => {
+	// console.log(props);
 	let plantInfoCardSection =  (
 		<section className="plantInfoCard">
 			<ul>
-				{props.plantsArray.map((plant) => {
-					return (
-						<li key={plant.id}>
-							<h3>Plant Name: {plant.name}</h3>
-							<p>Light Requirements: {plant.light}</p>
-							<p>Watering Requirements: {plant.water}</p>
-							<p>Soil Requirements: {plant.soil}</p>
-							<p>Plant Type: {plant.type}</p>
-							<img src={plant.firebasePicPath} alt="Picture of a plant"/>
-							<button onClick={() => props.removeItem(plant.id)}>Remove this plant from your garden!</button>
-						</li>
-					);
-				})}
+				<h2>{props.currentPlant.name}</h2>
+				<li>
+					<h4>Light Requirements: </h4>
+					<h5>{props.currentPlant.light}</h5>
+				</li>
+				<li>
+					<h4>Watering Requirements: </h4>
+					<h5>{props.currentPlant.water}</h5>
+				</li>
+				<li>
+					<h4>Soil Requirements: </h4>
+					<h5>{props.currentPlant.soil}</h5>
+				</li>
+				<li>
+					<h4>Plant Type: </h4>
+					<h5>{props.currentPlant.type}</h5>
+				</li>
+				<li>
+					<h4>Picture: </h4>
+					<img src={props.currentPlant.firebasePicPath} alt="Picture of a plant"/>
+				</li>
+				<li>
+					<button onClick={() => props.removeItem(props.currentPlant.id)}>Remove this plant from your garden</button>
+					<button className="newPlantButton" onClick={props.addNewPlant}>Add another plant to your garden</button>
+				</li>
 			</ul>
 		</section>
 	)
 	let plantFormSection = (
-		<section className='plantForm'>
-			<form onSubmit={props.handleSubmit} action="" method="post">
-				<h2>New Plant Form</h2>
+		<section className="plantForm__Section">
+			<form className="plantForm" onSubmit={props.handleSubmit} action="" method="post">
+				<h2>Add a new plant here!</h2>
 				<p>Fill out the form below to add a new plant to your garden.</p>
-				<input type="text" name='plantName' placeholder='Enter Plant Name' value={props.plantName} onChange={props.handleChange} />
-				<input type="text" name='plantLight' placeholder='Enter Light Requirements' value={props.plantLight} onChange={props.handleChange} />
-				<input type="text" name='plantWater' placeholder='Enter Water Requirements' value={props.plantWater} onChange={props.handleChange} />
-				<input type="text" name='plantSoil' placeholder='Enter Soil Requirements' value={props.plantSoil} onChange={props.handleChange} />
-				<label htmlFor="plantType">Select the type of plant:</label>
-				<select name="plantType" id="" onChange={props.handleChange} value={props.value}>
-					<option value="flower">Flower</option>
-					<option value="herb">Herb</option>
-					<option value="vegfru">Veggie or Fruit</option>
-				</select>
-				<label htmlFor="plantPic"> Select a picture of your plant:</label>
-				<input id="fileItem" type="file" name="plantPic" accept="image/*" value={props.plantPic} onChange={props.handleUpload} />
-				{props.loading ? 
-					<div>You're image is loading</div>
-				: 
-					<button>Add to garden!</button>
-				}
+				<ul>
+					<li>
+						<label htmlFor="plantName">Plant Name</label>
+						<input type="text" name="plantName" value={props.plantName} onChange={props.handleChange} />
+						<span>Eg: String of pearls</span>
+					</li>
+					<li>
+						<label htmlFor="plantLight">Light Requirements</label>
+						<textarea className="textarea" name="plantLight" value={props.plantLight} onChange={(e) => {
+								props.handleChange(e);
+								props.adjustTextarea();
+							}}></textarea>
+						<span>Eg: bright, indirect light</span>
+					</li>
+					<li>
+						<label htmlFor="plantWater">Water Requirements</label>
+						<textarea className="textarea" name="plantWater" value={props.plantWater} onChange={(e) => {
+								props.handleChange(e);
+								props.adjustTextarea();
+							}}></textarea>
+						<span>Eg: Once a week or when leaves get soft. Don't over water!</span>
+					</li>
+					<li>
+						<label htmlFor="plantSoil">Soil Requirements</label>
+						<textarea className="textarea" name="plantSoil" value={props.plantSoil} onChange={(e) => {
+								props.handleChange(e);
+								props.adjustTextarea();
+							}}></textarea>
+						<span>Eg. Well draining soil </span>
+					</li>
+					<li>
+						<label htmlFor="plantType">Type of plant:</label>
+						<select name="plantType" id="" onChange={props.handleChange} value={props.value}>
+							<option value="" selected disabled hidden>Select the type here</option>
+							<option value="flower">Flower</option>
+							<option value="herb">Herb</option>
+							<option value="vegfru">Veggie or Fruit</option>
+						</select>
+					</li>
+					<li>
+						<label htmlFor="plantPic"> Select a picture of your plant:</label>
+						<input id="fileItem" type="file" name="plantPic" accept="image/*" value={props.plantPic} onChange={props.handleUpload} />
+					</li>
+					<li className="submit__button">
+						{props.loading ? 
+							<p>You're image is loading...</p>
+						: 
+							<button>Add to garden!</button>
+						}
+					</li>
+				</ul>
 			</form>
 		</section>
 	)
@@ -124,12 +175,14 @@ class App extends React.Component {
 			loading: false, 
 			gardenEmpty: true,
 			plantInfoRequest: false, 
+			currentPlant: {}
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleUpload = this.handleUpload.bind(this);
 		this.removeItem = this.removeItem.bind(this);
 		this.handleInfoCardDisplay = this.handleInfoCardDisplay.bind(this);
+		this.addNewPlant = this.addNewPlant.bind(this);
 	}
 	handleUpload(event) {
 		this.setState({
@@ -178,12 +231,30 @@ class App extends React.Component {
 	removeItem(key) {
 		const plantRef = firebase.database().ref(`/plants/${key}`)
 		plantRef.remove();
-	}
-	handleInfoCardDisplay() {
-		// const plantRef = firebase.database().ref(`/plants/${key}`)
 		this.setState({
-			plantInfoRequest: true
+			currentPlant: {},
+			plantInfoRequest: false,
 		})
+	}
+	addNewPlant(event) {
+		this.setState({
+			plantInfoRequest: false,
+		})
+	}
+	handleInfoCardDisplay(id) {
+		const thePlant = this.state.plantsArray.find((plant) => {
+			return plant.id === id
+		});
+		this.setState({
+			currentPlant: thePlant,
+			plantInfoRequest: true,
+		})
+	}
+	adjustTextarea() {
+		const textareas = document.getElementsByClassName("textarea");
+		[...textareas].forEach((textarea) => {
+			textarea.style.height = (textarea.scrollHeight)+"px";
+		});
 	}
 	componentDidMount() {
 		dbRef.on('value', (snapshot) => {
@@ -226,6 +297,8 @@ class App extends React.Component {
 					handleSubmit={this.handleSubmit}
 					handleUpload={this.handleUpload}
 					handleInfoCardDisplay={this.handleInfoCardDisplay}
+					adjustTextarea={this.adjustTextarea}
+					addNewPlant={this.addNewPlant}
 
 					plantsArray={this.state.plantsArray}
 					plantName={this.state.plantName}
@@ -236,6 +309,7 @@ class App extends React.Component {
 					plantPic={this.state.plantPic}
 					loading={this.state.loading}
 					plantInfoRequest={this.state.plantInfoRequest}
+					currentPlant={this.state.currentPlant}
 				/>
 			</div>
 		);
